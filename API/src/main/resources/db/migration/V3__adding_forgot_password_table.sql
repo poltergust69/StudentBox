e73fbf0a24fc2fc484b9ec2363610a35690ef953
partaloski <ppartaloski@yahoo.com>
2023-03-26 17:25:49 +0200

    Improving script, adding forgotten User FK reference

--- API/src/main/resources/db/migration/V3__adding_forgot_password_table.sql
+++ API/src/main/resources/db/migration/V3__adding_forgot_password_table.sql
@@ -3,5 +3,6 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE TABLE user_forgot_password_requests(
     user_id uuid primary key,
     created_at timestamp not null default now(),
-    secret_code varchar(100)
+    secret_code varchar(100),
+    constraint fk_forgotpassword_user foreign key (user_id) references users(id)
 );
